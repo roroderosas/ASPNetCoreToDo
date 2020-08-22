c4e7e49c0f0f6b6853047d15a715adbfbe1f3ec2
MinhDC1 <duongcongminh@gmail.com>
2020-08-21 19:41:24 -0600

    initial commit.

---
+++ ASPNetCoreToDo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNetCoreToDo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
